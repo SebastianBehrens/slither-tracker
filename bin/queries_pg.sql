0b1da9329f75a06539e132a866a691d9a6c7e926
Sebastian Behrens <seb.beh@outlook.com>
2025-03-23 19:42:53 +0100

    feat: track queries

--- bin/queries_pg.sql
+++ bin/queries_pg.sql
@@ -1,2 +1,36 @@
 SELECT * FROM public.server_user_rank
+SELECT DISTINCT created_at FROM public.server_user_rank
+SELECT * FROM public.user_rank_runs
+SELECT * FROM public.server_user_rank LIMIT 3
+TRUNCATE public.server_user_rank
 
+TRUNCATE TABLE public.user_run
+SELECT * FROm public.user_run
+
+
+SELECT *
+FROM user_run
+WHERE 1=1
+AND duration_seconds IS NOT NULL
+AND nick NOT LIKE ''
+ORDER BY duration_seconds DESC LIMIT 1000;
+
+SELECT
+*
+FROM
+user_run
+WHERE duration_seconds IS NOT NULL
+AND nick != 'NTL.bot'
+ORDER BY max_score DESC LIMIT 1000;
+
+SELECT
+min_rank
+,nick
+,duration_seconds
+,start_time
+FROM public.user_run
+WHERE duration_seconds IS NOT NULL
+AND nick NOT LIKE ''
+AND nick NOT LIKE '%NTL.bot%'
+ORDER BY duration_seconds DESC
+LIMIT 3;
